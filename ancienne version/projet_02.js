22f86446ac1fff4c8d308a872824fc112ed3e245
MSI\Student <lauwers.contact@gmail.com>
2017-11-08 11:30:49 +0100

    modifications of names for it to be more visible

---
+++ ancienne version/projet_02.js
@@ -0,0 +1,24 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function functioncss() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.drop-btn')) {
+
+    let dropdowns = document.getElementsByClassName("drop-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// Onglets:
+
+
