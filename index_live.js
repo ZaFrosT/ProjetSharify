bfc9bdf9439de050e12e3d4c0744736bf6185e8f
MSI\Student <lauwers.contact@gmail.com>
2017-11-09 10:48:37 +0100

    fix of video and t'chat's twitch (responsive)

--- index_live.js
+++ index_live.js
@@ -1,24 +1,8 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function functioncss() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.drop-btn')) {
-
-    let dropdowns = document.getElementsByClassName("drop-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-// Onglets:
-
-
+let options = {
+  width: 70%,
+  height: 80%,
+  channel: "{7xSev}",
+  //video: "{VIDEO_ID}"
+};
+let player = new Twitch.Player("{PLAYER_DIV_ID}", options);
+player.setVolume(0.5);
